21cf05dec137ac71f8910249c15769a2cb4c80af
Rafal <piastka.rafal@gmail.com>
2020-11-25 20:47:31 +0100

    InheritanceAdded

--- src/main/java/com/javastart/library/model/Book.java
+++ src/main/java/com/javastart/library/model/Book.java
@@ -1,31 +1,50 @@
 package com.javastart.library.model;
 
-public class Book {
-    String title;
-    String author;
-    int releaseDate;
-    int pages;
-    String publisher;
-    String isbn;
+public class Book extends Publication {
+    // Pola
+    private String author;
+    private int pages;
+    private String isbn;
 
-    public Book(String title, String author, int releaseDate,
-                int pages, String publisher, String isbn) {
-        this(title, author, releaseDate, pages, publisher);
+    // Konstruktory
+    public Book(String title, String author, int year, int pages, String publisher,
+                String isbn) {
+        setTitle(title);
+        setPublisher(publisher);
+        setYear(year);
+        this.pages = pages;
+        this.author = author;
         this.isbn = isbn;
     }
 
-    Book(String title, String author, int releaseDate, int pages, String publisher) {
-        this.title = title;
-        this.author = author;
-        this.releaseDate = releaseDate;
-        this.pages = pages;
-        this.publisher = publisher;
+    // settery i gettery
+    public String getIsbn() {
+        return isbn;
     }
 
-    void printInfo() {
-        String info = title + "; " + author + "; " + releaseDate + "; "
-                + pages + "; " + publisher + "; " + isbn;
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void printInfo() {
+        String info = getTitle() + "; " + author + "; " + getYear() + "; "
+                + pages + "; " + getPublisher() + "; " + isbn;
         System.out.println(info);
     }
-
 }
